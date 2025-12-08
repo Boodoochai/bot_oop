ee8ca31da6d27d414531ede84c74d8060dc6fd15
Boodoochai <itstoomymail@mail.ru>
2025-12-08 15:40:57 +0500

    Add assert on null args in constructor and equals and hashcode methods.

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,10 +1,15 @@
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.util.Objects;
+
 final public class Request {
     private @NonNull final Client requestOwner;
     private @NonNull final String text;
 
     Request(@NonNull final Client requestOwner, @NonNull final String requestText) {
+        if (requestOwner == null || requestText == null) {
+            throw new NullPointerException();
+        }
         this.requestOwner = requestOwner;
         this.text = requestText;
     }
@@ -15,5 +20,18 @@ final public class Request {
 
     public @NonNull String getText() {
         return text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Request)) return false;
+        Request request = (Request) o;
+        return requestOwner.equals(request.requestOwner) && text.equals(request.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requestOwner, text);
     }
 }
